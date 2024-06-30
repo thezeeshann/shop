57d69d16136ed40f9bf32c88ddb64c1f05326242
thezeeshann <info.zeeshann@gmail.com>
2024-06-30 18:45:47 +0530

    feat: Add login & register zod schema

---
+++ src/lib/validations/login.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters long",
+    })
+    .max(100, {
+      message: "Password must be at most 100 characters long",
+    }),
+  code: z.optional(z.string()),
+});
