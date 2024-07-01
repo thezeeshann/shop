4eeeef2e7fea09c164a702ad93a19914836e885d
thezeeshann <info.zeeshann@gmail.com>
2024-07-01 17:43:49 +0530

    refactor: update authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "./validations/login";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -13,6 +15,29 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      authorize: async (credentials) => {
+        const validatedFields = loginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await prisma.user.findFirst({
+            where: {
+              email: email,
+            },
+          });
+          if (!user || !user.password) return null;
+
+          return user;
+        }
+        return null;
+      },
     }),
   ],
 });
