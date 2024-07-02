663b6370021d40455e4aeb31d610dc94d8fa3830
thezeeshann <info.zeeshann@gmail.com>
2024-07-02 17:26:53 +0530

    refactor: update authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,10 +7,49 @@ import { loginSchema } from "./validations/login";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
   session: {
     strategy: "jwt",
   },
-  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (session && token.sub) {
+        session.user.id = token.sub;
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role as string;
+      }
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+        session.user.image = token.image as string;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await prisma.user.findFirst({
+        where: {
+          id: token.sub,
+        },
+      });
+      if (!existingUser) return token;
+      const existingAccount = await prisma.account.findFirst({
+        where: {
+          userId: existingUser.id,
+        },
+      });
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.twoFactorEnabled;
+      token.image = existingUser.image;
+      return token;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
