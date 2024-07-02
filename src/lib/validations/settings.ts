e04d78779de043669db76bfaf71ff37becf90231
thezeeshann <info.zeeshann@gmail.com>
2024-07-02 17:31:36 +0530

    feat: Added settings schema

---
+++ src/lib/validations/settings.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+export const settingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    image: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(8)),
+    newPassword: z.optional(z.string().min(8)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false;
+      }
+      return true;
+    },
+    { message: "New password is required", path: ["newPassword"] }
+  );
