f7233257aebb4ed33df2704267bbae74be5cc1be
thezeeshann <info.zeeshann@gmail.com>
2024-07-03 01:39:51 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {protocol:"https" ,hostname:"lh3.googleusercontent.com"}
-        ]
-    }
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "utfs.io" },
+    ],
+  },
 };
 
 export default nextConfig;
